b3c0f359f758a329073964ec5a8964832a20a94c
Jackson <jackson.rodrigues9@hotmail.com>
2019-05-14 15:21:21 -0300

    Alteração de array

---
+++ Detecção de mudança de array/script.js
@@ -0,0 +1,21 @@
+let app = new Vue({
+	el: '#app',
+	data: {
+		lista: [{
+				name: 'Jackson'
+			},
+			{
+				name: 'Lucas'
+			},
+			{
+				name: 'Fernanda'
+			},
+		],
+		listas: {
+			name: 'Jackson',
+			age: 19,
+			date: "13/08/1999"
+		}
+	}
+
+});
