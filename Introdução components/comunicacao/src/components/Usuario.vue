c5f121e9b9c191e8f9e85e2bba8e0335e11f2578
Jackson <jackson.rodrigues9@hotmail.com>
2019-05-17 14:11:09 -0300

    Adicionado exercício de user

---
+++ Introdução components/comunicacao/src/components/Usuario.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <h1>Componente Usuário</h1>
+    <p>Esse é um componente muito legal!</p>
+    <hr>
+    <div class="componentes">
+      <app-usuario-info user="user"/>
+      <app-usuario-editar user="user"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import AppUsuarioInfo from "./UsuarioInfo";
+import AppUsuarioEditar from "./UsuarioEditar";
+
+export default {
+  data() {
+    return {
+      user: {
+        nome: "Jackson Silva Rodrigues",
+        age: 19,
+        birthday: "13/08/1999"
+      }
+    };
+  },
+  components: { AppUsuarioInfo, AppUsuarioEditar }
+};
+</script>
+
+<style scoped>
+.container {
+  background-color: #27363b;
+  color: #fff;
+  padding: 10px;
+}
+
+.container hr {
+  margin: 20px 10px;
+}
+
+.componentes {
+  display: flex;
+}
+
+.componentes > * {
+  margin: 10px;
+}
+</style>
---
+++ Introdução components/comunicacao/src/components/UsuarioInfo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="componente">
+    <h2>As Informações de Usuário</h2>
+    <p v-for="(value,key,index) in user">Nome: {{value}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["user"]
+};
+</script>
+
+<style scoped>
+.componente {
+  flex: 1;
+  background-color: #ec485f;
+  color: #fff;
+}
+</style>
