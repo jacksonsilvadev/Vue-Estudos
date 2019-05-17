1a5ca79d47dbfb7e7b17c599491877009c01efbe
Jackson <jackson.rodrigues9@hotmail.com>
2019-05-17 13:54:21 -0300

    Adicionando o componente de Desafio

---
+++ Introdução components/componente-desafio/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <nav-bar></nav-bar>
+    <tool-bar></tool-bar>
+    <carousel></carousel>
+    <footer></footer>
+  </v-app>
+</template>
+
+<script>
+import NavBar from "./NavBarComponent";
+import ToolBar from "./ToolbarComponent";
+import Carousel from "./CarouselComponent";
+import Footer from "./FooterComponent";
+
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    NavBar,
+    ToolBar,
+    Carousel,
+    Footer
+  }
+};
+</script>
