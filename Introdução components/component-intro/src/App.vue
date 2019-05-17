e2d030d4d1405e7686dc3ea8c9c4c3a708dda69a
Jackson <jackson.rodrigues9@hotmail.com>
2019-05-17 08:58:30 -0300

    começando com o vue cli e components

---
+++ Introdução components/component-intro/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <contador></contador>
+  </div>
+</template>
+
+<script>
+import Contador from "./Contador";
+
+export default {
+  data() {
+    return {
+      contador: 0
+    };
+  },
+  components: {
+    Contador
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ Introdução components/component-intro/src/Contador.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="contador">
+    <div>
+      <h1>Contador: {{ contador }}</h1>
+      <button @click="adicionar">+</button>
+      <button @click="subtrair">-</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      contador: 0
+    };
+  },
+  methods: {
+    adicionar() {
+      return this.contador++;
+    },
+    subtrair() {
+      return this.contador > 0 ? this.contador-- : "";
+    }
+  }
+};
+</script>
+
+<style scoped>
+.contador {
+  display: flex;
+}
+.contador button {
+  padding: 5px;
+  margin: 5px;
+  background-color: #000;
+  color: #fff;
+  border: 1px #fff dashed;
+}
+</style>
