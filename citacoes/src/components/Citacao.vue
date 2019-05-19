6de3515fde7ed5cd74479ccd6cedd56178301ec8
Jackson Silva <jackson.silva@minds4health.com.br>
2019-05-19 20:05:03 -0300

    Adicionado aula de slot

---
+++ citacoes/src/components/Citacao.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="citacao">
+       <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['citacao'],
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+</style>
