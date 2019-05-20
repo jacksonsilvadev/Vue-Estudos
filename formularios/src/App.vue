8b0e9890d5bc00bd4cef484b4ba5279ee85e8516
Jackson <jackson.rodrigues9@hotmail.com>
2019-05-20 09:42:18 -0300

    Checkbox valores multiplos

---
+++ formularios/src/App.vue
@@ -0,0 +1,161 @@
+<template>
+  <div id="app">
+    <h1>Registrar Reclamação</h1>
+    <div class="conteudo">
+      <form class="painel">
+        <div class="cabecalho">Formulário</div>
+        <Rotulo nome="E-mail">
+          <input type="text" v-model.lazy.trim="usuario.email">
+        </Rotulo>
+        <Rotulo nome="Senha">
+          <input type="password" v-model="usuario.password">
+        </Rotulo>
+        <Rotulo nome="Idade">
+          <input type="number" v-model.number="usuario.age">
+        </Rotulo>
+        <Rotulo nome="Mensagem">
+          <textarea name cols="30" rows="5" v-model="mensagem"></textarea>
+        </Rotulo>
+        <Rotulo nome="Características do Problema">
+          <span class="mr-4">
+            <input type="checkbox" v-model="caracteristicas" value="reproduzivel"> Reproduzível
+          </span>
+          <span>
+            <input type="checkbox" v-model="caracteristicas" value="intermitente"> Intermitente
+          </span>
+        </Rotulo>
+        <Rotulo nome="Qual produto?">
+          <span class="mr-4">
+            <input type="radio"> Web
+          </span>
+          <span class="mr-4">
+            <input type="radio"> Mobile
+          </span>
+          <span>
+            <input type="radio"> Outro
+          </span>
+        </Rotulo>
+        <Rotulo nome="Prioridade">
+          <select name id>
+            <option></option>
+          </select>
+        </Rotulo>
+        <Rotulo nome="Primeira Reclamação?">
+          <Escolha/>
+        </Rotulo>
+        <hr>
+        <button>Enviar</button>
+      </form>
+      <div class="painel">
+        <div class="cabecalho">Resultado</div>
+        <Rotulo nome="E-mail">
+          <span>{{usuario.email}}</span>
+        </Rotulo>
+        <Rotulo nome="Senha">
+          <span>{{ usuario.password }}</span>
+        </Rotulo>
+        <Rotulo nome="Idade">
+          <span>{{ usuario.age }}</span>
+        </Rotulo>
+        <Rotulo nome="Mensagem">
+          <span style="white-space: pre">{{mensagem}}</span>
+        </Rotulo>
+        <Rotulo nome="Marque as Opções">
+          <span>
+            <ul>
+              <li v-for="c in caracteristicas" :key="c">{{c}}</li>
+            </ul>
+          </span>
+        </Rotulo>
+        <Rotulo nome="Qual produto?">
+          <span>???</span>
+        </Rotulo>
+        <Rotulo nome="Prioridade">
+          <span>???</span>
+        </Rotulo>
+        <Rotulo nome="Primeira Reclamação?">
+          <span>???</span>
+        </Rotulo>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Rotulo from "./components/Rotulo.vue";
+import Escolha from "./components/Escolha.vue";
+
+export default {
+  name: "app",
+  components: { Rotulo, Escolha },
+  data() {
+    return {
+      mensagem: "",
+      caracteristicas: [],
+      usuario: {
+        email: "",
+        password: "",
+        age: 19
+      }
+    };
+  }
+};
+</script>
+
+<style>
+body {
+  background-color: #ececec;
+}
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+
+  display: flex;
+  flex-direction: column;
+}
+
+.conteudo {
+  display: flex;
+}
+
+.painel {
+  flex: 1;
+  background: #fff;
+  margin: 0px 10px;
+  padding: 20px;
+  border: 1px solid #aaa;
+  border-radius: 5px;
+}
+
+.painel .cabecalho {
+  width: 100%;
+  background-color: #ddd;
+  padding: 10px 0px;
+  border-radius: 5px;
+  font-size: 1.4rem;
+}
+
+#app form button {
+  float: right;
+  margin: 10px 0px;
+  padding: 10px 20px;
+  font-size: 1.4rem;
+  border-radius: 5px;
+  color: #fff;
+  background-color: #2196f3;
+}
+
+#app h1 {
+  font-weight: 200;
+  margin: 20px;
+  padding: 0;
+}
+
+.mr-4 {
+  margin-right: 40px;
+}
+</style>
