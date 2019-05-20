dd34b2bb17095b238930650ed9beacd796cd3a07
Jackson Silva <jackson.silva@minds4health.com.br>
2019-05-20 20:52:43 -0300

    Multiplos filtros e filter global

---
+++ filtros-mixins-exercicios/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+	<div id="app">
+		<h1>Filtros & Mixins</h1>
+		<hr>
+		<!-- Passando pipe para chamar um filter -->
+		<p>{{ cpf | cpf | inverter}}</p>
+	</div>
+</template>
+
+<script>
+export default {
+	filters: {
+		cpf(valor) {
+			const arr = valor.split('');
+			arr.splice(3,0,'.')
+			arr.splice(7,0,'.')
+			arr.splice(11,0,'-')
+			return arr.join('')
+		}
+	},
+		data() {
+			return {
+				cpf: '60070080090'
+			}
+		}
+	}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 2.5rem;
+}
+</style>
