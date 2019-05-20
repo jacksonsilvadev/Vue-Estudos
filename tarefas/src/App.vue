9c43781f0f013fb6afb584424e3fe8a5af664fb1
Jackson <jackson.rodrigues9@hotmail.com>
2019-05-20 09:07:20 -0300

    Projeto finalizado TODO de components

--- tarefas/src/App.vue
+++ tarefas/src/App.vue
@@ -1,78 +1,100 @@
 <template>
-	<div id="app">
-		<h1>Tarefas</h1>
-		<progress-bar :percentage="percentage"></progress-bar>
-		<add-list @addTaskToFather="addToList"></add-list>
-		<list-tasks @changeToCompleted="changeTask" :lists="lists"></list-tasks>
-	</div>
+  <div id="app">
+    <h1>Tarefas</h1>
+    <progress-bar :percentage="percentage"></progress-bar>
+    <add-list @addTaskToFather="addToList"></add-list>
+    <list-tasks @callRemove="removeTask" @changeToCompleted="changeTask" :lists="lists"></list-tasks>
+  </div>
 </template>
 
 <script>
-import ProgressBar from './components/ProgressBar'
-import AddList from './components/AddList'
-import ListTasks from './components/ListTasks'
+import ProgressBar from "./components/ProgressBar";
+import AddList from "./components/AddList";
+import ListTasks from "./components/ListTasks";
 export default {
-	data() {
-		return {
-			percentage: 20,
-			lists: [
-				{name: 'Lavar louça', completed: false},
-				{name: 'Lavar louça', completed: false}
-				]
-		}
-	},
-	components: {
-		ProgressBar, AddList,ListTasks
-	},
-	methods: {
-		addToList(task) {
-			this.lists.push(task)
-		},
-		changeTask(index) {
+  data() {
+    return {
+      lists: []
+    };
+  },
+  components: {
+    ProgressBar,
+    AddList,
+    ListTasks
+  },
+  watch: {
+    lists: function(novo, antigo) {
+      return localStorage.setItem("lists", JSON.stringify(this.lists));
+    }
+  },
+  computed: {
+    percentage() {
+      const total = this.lists.length;
+      const done = this.lists.filter(t => t.completed).length;
 
-			this.lists[index].completed = !this.lists[index].completed
-		}
-	}
-}
+      return Math.round((done / total) * 100 || 0);
+    }
+  },
+  methods: {
+    addToList(task) {
+      var validation = this.lists.filter(item => {
+        return item.name == task.name;
+      });
+
+      if (validation == "") return this.lists.push(task);
+    },
+    changeTask(index) {
+      return (this.lists[index].completed = !this.lists[index].completed);
+    },
+    removeTask(i) {
+      return this.lists.splice(i, 1);
+    }
+  },
+  created() {
+    var lista = JSON.parse(localStorage.getItem("lists"));
+    this.lists = lista;
+  }
+};
 </script>
 
 <style>
-	body {
-		font-family: 'Lato', sans-serif;
-		background: linear-gradient(to right, rgb(22, 34, 42), rgb(58, 96, 115));
-		color: #FFF;
-	}
+body {
+  font-family: "Lato", sans-serif;
+  background: linear-gradient(to right, rgb(22, 34, 42), rgb(58, 96, 115));
+  color: #fff;
+}
 
-	#app {
-		display: flex;
-		flex: 1;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		height: 100vh;
-	}
+#app {
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
 
-	.newtask, .bar {
-		margin-top: 20px;
-	}
+.newtask,
+.bar {
+  margin-top: 20px;
+}
 
-	#app h1 {
-		margin-bottom: 5px;
-		font-weight: 300;
-		font-size: 3rem;
-	}
+#app h1 {
+  margin-bottom: 5px;
+  font-weight: 300;
+  font-size: 3rem;
+}
 
-	.bar {
-		width: 80%;
-		height: 20px;
-		border: 1px solid #AAA;
-		border-radius: 10px;
-	}
-	.bar .percentage {
-		display: flex;
-		justify-content: center;
-		height: 100%;
-		background-color: green;
-		border-radius: 10px;
-	}
+.bar {
+  width: 80%;
+  height: 20px;
+  border: 1px solid #aaa;
+  border-radius: 10px;
+}
+.bar .percentage {
+  display: flex;
+  justify-content: center;
+  height: 100%;
+  background-color: green;
+  border-radius: 10px;
+}
 </style>
--- tarefas/src/components/ListTasks.vue
+++ tarefas/src/components/ListTasks.vue
@@ -1,101 +1,107 @@
 <template>
-    <div class="task-grid">
-        <div  class="task" :class="[{'pending': !item.completed}, {'done': item.completed}]" v-for="(item,index) in lists" :key="index">
-            <span class="close">x</span>
-            <p @click="change(index)">{{item.name}}</p>
-        </div>
+  <div class="task-grid">
+    <div
+      class="task"
+      :class="[{'pending': !item.completed}, {'done': item.completed}]"
+      v-for="(item,index) in lists"
+      :key="index"
+    >
+      <span @click="removeTaks(index)" class="close">x</span>
+      <p @click="change(index)">{{item.name}}</p>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    props: ['lists'],
-    methods: {
-        change(index) {
-            
-            this.$emit('changeToCompleted',index)
-        }
+  props: ["lists"],
+  methods: {
+    change(index) {
+      this.$emit("changeToCompleted", index);
+    },
+    removeTaks(i) {
+      this.$emit("callRemove", i);
     }
-}
+  }
+};
 </script>
 
 <style>
 .task-grid {
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
-    -webkit-box-pack: center;
-    -ms-flex-pack: center;
-    justify-content: center;
-    -ms-flex-wrap: wrap;
-    flex-wrap: wrap;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  -ms-flex-wrap: wrap;
+  flex-wrap: wrap;
 }
 
 .task-grid .task {
-    margin: 10px;
+  margin: 10px;
 }
 
 .done {
-    color: #DDD;
-    border-left: 12px solid #0A8F08;
-    background-color: #4CAF50;
-    text-decoration: line-through;
+  color: #ddd;
+  border-left: 12px solid #0a8f08;
+  background-color: #4caf50;
+  text-decoration: line-through;
 }
 
 .task {
-    position: relative;
-    -webkit-box-sizing: border-box;
-    box-sizing: border-box;
-    width: 350px;
-    height: 150px;
-    padding: 10px;
-    border-radius: 8px;
-    font-size: 2rem;
-    font-weight: 300;
-    cursor: pointer;
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
-    -webkit-box-pack: center;
-    -ms-flex-pack: center;
-    justify-content: center;
-    -webkit-box-align: center;
-    -ms-flex-align: center;
-    align-items: center;
+  position: relative;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  width: 350px;
+  height: 150px;
+  padding: 10px;
+  border-radius: 8px;
+  font-size: 2rem;
+  font-weight: 300;
+  cursor: pointer;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
 }
 
 .done .close {
-    background-color: #0A8F08;
+  background-color: #0a8f08;
 }
 
 .close {
-    position: absolute;
-    right: 10px;
-    top: 10px;
-    font-size: 0.9rem;
-    font-weight: 600;
-    height: 20px;
-    width: 20px;
-    border-radius: 10px;
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
-    -webkit-box-pack: center;
-    -ms-flex-pack: center;
-    justify-content: center;
+  position: absolute;
+  right: 10px;
+  top: 10px;
+  font-size: 0.9rem;
+  font-weight: 600;
+  height: 20px;
+  width: 20px;
+  border-radius: 10px;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
 }
 
 .pending .close {
-    background-color: #B73229;
+  background-color: #b73229;
 }
 
 .pending {
-    border-left: 12px solid #B73229;
-    background-color: #F44336;
+  border-left: 12px solid #b73229;
+  background-color: #f44336;
 }
-
 </style>
