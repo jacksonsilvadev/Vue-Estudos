d38d270f4ff1ca17bd31332ce759974f63ede5c4
Jackson <jackson.rodrigues9@hotmail.com>
2019-05-20 17:57:38 -0300

    Enviando formulário

--- formularios/src/App.vue
+++ formularios/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <h1>Registrar Reclamação</h1>
     <div class="conteudo">
-      <form class="painel">
+      <form class="painel" v-if="!enviado">
         <div class="cabecalho">Formulário</div>
         <Rotulo nome="E-mail">
           <input type="text" v-model.lazy.trim="usuario.email">
@@ -49,9 +49,9 @@
           <Escolha v-model="escolha"/>
         </Rotulo>
         <hr>
-        <button>Enviar</button>
+        <button @click.prevent="enviar">Enviar</button>
       </form>
-      <div class="painel">
+      <div class="painel" v-else>
         <div class="cabecalho">Resultado</div>
         <Rotulo nome="E-mail">
           <span>{{usuario.email}}</span>
@@ -109,8 +109,14 @@ export default {
         { codigo: 2, nome: "Moderada" },
         { codigo: 3, nome: "Alta" }
       ],
-      escolha: true
+      escolha: true,
+      enviado: false
     };
+  },
+  methods: {
+    enviar() {
+      this.enviado = true;
+    }
   }
 };
 </script>
