876c739d3e9eb267efdb5f362f440a2e224a2620
Jackson <jackson.rodrigues9@hotmail.com>
2019-05-23 15:34:50 -0300

    Primeira animação de fade

--- animacoes-exercicios/src/App.vue
+++ animacoes-exercicios/src/App.vue
@@ -3,7 +3,12 @@
     <h1>Animações</h1>
     <hr>
     <b-button class="my-3" @click="exibir = !exibir" variant="primary">Mostrar mensagem</b-button>
-    <transition>
+
+    <transition name="fade">
+      <b-alert variant="info" v-if="exibir" show>{{ msg }}</b-alert>
+    </transition>
+
+    <transition name="slide">
       <b-alert variant="info" v-if="exibir" show>{{ msg }}</b-alert>
     </transition>
   </div>
@@ -29,5 +34,23 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
   font-size: 1.5rem;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 1s;
+}
+
+/* Por padrão opacity ja é 1 */
+.fade-enter-to {
+}
+
+/* Por padrão opacity ja é 1 */
+.fade-leave {
 }
 </style>
