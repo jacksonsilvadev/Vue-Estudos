fd5b15a61bdd50d1736a05933062b3c304580f98
Jackson <jackson.rodrigues9@hotmail.com>
2019-05-23 15:17:58 -0300

    Adicionado aula de transição

---
+++ animacoes-exercicios/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app" class="container-fluid">
+    <h1>Animações</h1>
+    <hr>
+    <b-button class="my-3" @click="exibir = !exibir" variant="primary">Mostrar mensagem</b-button>
+    <transition>
+      <b-alert variant="info" v-if="exibir" show>{{ msg }}</b-alert>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "Uma mensagem de informação para o usuário",
+      exibir: false
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 1.5rem;
+}
+</style>
