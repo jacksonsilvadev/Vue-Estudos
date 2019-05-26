8f0a8239039690283be1935db81379d48b464478
Jackson Silva <jackson.rodrigues9@hotmail.com>
2019-05-26 16:09:24 -0300

    Componente dinâmico com animação

--- animacoes-exercicios/src/App.vue
+++ animacoes-exercicios/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app" class="container-fluid">
     <h1>Animações</h1>
     <hr>
-    <b-button class="my-3" @click="exibir = !exibir" variant="primary">Mostrar mensagem</b-button>
+    <!-- <b-button class="my-3" @click="exibir = !exibir" variant="primary">Mostrar mensagem</b-button> -->
 
     <!-- <transition name="fade" appear>
       <b-alert variant="info" v-if="exibir" show>{{ msg }}</b-alert>
@@ -27,7 +27,7 @@
       <option value="fade">Fade</option>
       <option value="slide">Slide</option>
     </b-select>-->
-
+<!-- 
     <transition :name="tipoAnimação" appear>
       <b-alert variant="info" v-show="exibir" show>{{ msg }}</b-alert>
     </transition>
@@ -39,11 +39,13 @@
     >
       <b-alert variant="info" v-if="exibir" key="info" show>{{ msg }}</b-alert>
       <b-alert variant="warning" key="warn" v-else show>{{ msg }}</b-alert>
-    </transition>
+    </transition> -->
 
     <hr>
     <button @click="exibir2 = !exibir2">Mostrar</button>
+    <!-- :css desabilita o css dessa animação que vem por padrão -->
     <transition
+    
       :css="false"
       @before-enter="beforeEnter"
       @enter="enter"
@@ -56,27 +58,60 @@
     >
       <div v-if="exibir2" class="caixa"></div>
     </transition>
+
+    <hr>
+  <div class="mb-4">
+   <b-button class="mr-4" variant="primary" @click="componenteSelecionado = 'AlertaInfo'">Info</b-button>
+    <b-button variant="primary" @click="componenteSelecionado = 'AlertaAdvertencia'">Advertencia</b-button>
+  </div>
+   
+<transition name="fade" mode="out-in">
+  <component :is="componenteSelecionado"></component>
+</transition>
   </div>
 </template>
 
 
 
 <script>
+
+import AlertaInfo from './AlertaInfo.vue'
+import AlertaAdvertencia from './AlertaAdvertencia.vue'
+
 export default {
+  components: {
+    AlertaAdvertencia, AlertaInfo
+  },
   data() {
     return {
       msg: "Uma mensagem de informação para o usuário",
       exibir: true,
       exibir2: true,
-      tipoAnimação: ""
+      tipoAnimação: "fade",
+      larguraBase: 0,
+      componenteSelecionado: 'AlertaInfo'
     };
   },
   methods: {
+    animar(el, done, negativo) {
+      let rodada = 1
+      // Abrindo um temporizador para que ele aumente a cada nova rodada e quando a rodada for maior que 30 ele começa novamente a execução do 0
+      const temporizador = setInterval(() => {
+        const novaLargura = this.larguraBase + ( negativo ? -rodada * 10: rodada * 10 )
+      el.style.width = `${novaLargura}px`
+      rodada++
+      if(rodada > 30) {
+        clearInterval(temporizador)
+        done()
+      }
+      }, 20)
+    },
     beforeEnter(el) {
-      console.log("beforeEnter");
+       this.larguraBase = 0
+        el.style.width = `${this.larguraBase}px`
     },
     enter(el, done) {
-      console.log("enter");
+     this.animar(el,done)
     },
     afterEnter(el) {
       console.log("afterEnter");
@@ -85,10 +120,13 @@ export default {
       console.log("enterCancelled");
     },
     beforeLeave(el) {
-      console.log("beforeLeave");
+      this.larguraBase = 300
+      el.style.width = `${this.larguraBase}px`
     },
-    leave(el) {
-      console.log("leave");
+    leave(el, done) {
+
+      this.animar(el, done, true)
+    
     },
     afterLeave(el) {
       console.log("afterLeave");
