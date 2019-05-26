fde35bbc7d4c55b58d11ea21300fe3261cd9a4f9
Jackson Silva <jackson.rodrigues9@hotmail.com>
2019-05-26 17:53:54 -0300

    Enviando um post para firebase

--- Projetos/http-exercicios/src/App.vue
+++ Projetos/http-exercicios/src/App.vue
@@ -1,11 +1,35 @@
 <template>
 	<div id="app" class="container">
 		<h1>HTTP com Axios</h1>
+	
+		<b-card>
+			<b-form-group label="Nome: ">
+				<b-form-input type="text" size="lg"
+				v-model="usuario.nome"
+				placeholder="Insira seu nome"></b-form-input>
+			</b-form-group>
+
+			<b-form-group label="E-mail: ">
+				<b-form-input type="email" size="lg"
+				v-model="usuario.email"
+				placeholder="Insira seu e-mail"></b-form-input>
+			</b-form-group>
+			<hr>
+			<b-button size="lg" variant="success" @click="salvar"> Salvar </b-button>
+		</b-card>
 	</div>
 </template>
 
 <script>
 export default {
+	data() {
+		return {
+			usuario: {
+				nome: '',
+				email: ''
+			}
+		}
+	},
 	// created() {
 	// 	this.$http.post('usuario.json', {
 	// 		nome: 'Maria',
@@ -13,7 +37,17 @@ export default {
 	// 	}).then(res => {
 	// 		console.log(res)
 	// 	})
-	// }
+	// },
+	methods: {
+		salvar() {
+			this.$http.post('usuario.json', this.usuario)
+			.then(res => {
+				this.usuario.nome = ''
+				this.usuario.email = ''
+			})
+			.catch(err => console.log(err.message))
+		}
+	}
 }
 </script>
 
