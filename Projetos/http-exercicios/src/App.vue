2c6a3c2d27bdb770d5a0f1153e38314fddbe5263
Jackson Silva <jackson.rodrigues9@hotmail.com>
2019-05-26 18:48:22 -0300

    Alterado mensagens validação

--- Projetos/http-exercicios/src/App.vue
+++ Projetos/http-exercicios/src/App.vue
@@ -1,32 +1,37 @@
 <template>
 	<div id="app" class="container">
 		<h1>HTTP com Axios</h1>
-	
+		<b-alert show dismissible v-for="mensagem in mensagens"
+			:key="mensagem.texto"
+			:variant="mensagem.tipo">{{ mensagem.texto }}</b-alert>
 		<b-card>
-			<b-form-group label="Nome: ">
+			<b-form-group label="Nome:">
 				<b-form-input type="text" size="lg"
-				v-model="usuario.nome"
-				placeholder="Insira seu nome"></b-form-input>
+					v-model="usuario.nome"
+					placeholder="Informe o Nome"></b-form-input>
 			</b-form-group>
-
-			<b-form-group label="E-mail: ">
+			<b-form-group label="E-mail:">
 				<b-form-input type="email" size="lg"
-				v-model="usuario.email"
-				placeholder="Insira seu e-mail"></b-form-input>
+					v-model="usuario.email"
+					placeholder="Informe o E-mail"></b-form-input>
 			</b-form-group>
 			<hr>
-			<b-button size="lg" variant="success" @click="salvar"> Salvar </b-button>
-
-			<b-button size="lg" class="ml-2" variant="info" @click="obterUsuarios"> Obter Usuários </b-button>
+			<b-button @click="salvar"
+				size="lg" variant="primary">Salvar</b-button>
+			<b-button @click="obterUsuarios"
+				size="lg" variant="success"
+				class="ml-2">Obter Usuários</b-button>
 		</b-card>
-
 		<hr>
-		
 		<b-list-group>
 			<b-list-group-item v-for="(usuario, id) in usuarios" :key="id">
-				<strong>Nome: </strong> {{ usuario.nome }} <br/>
-				<strong>Email: </strong> {{ usuario.email }} <br/>
-				<strong>ID: </strong> {{ id }} <br/>
+				<strong>Nome: </strong> {{ usuario.nome }}<br>
+				<strong>E-mail: </strong> {{ usuario.email }}<br>
+				<strong>ID: </strong> {{ id }}<br>
+				<b-button variant="warning" size="lg"
+					@click="carregar(id)">Carregar</b-button>
+				<b-button variant="danger" size="lg" class="ml-2"
+					@click="excluir(id)">Excluir</b-button>
 			</b-list-group-item>
 		</b-list-group>
 	</div>
@@ -36,40 +41,62 @@
 export default {
 	data() {
 		return {
+			mensagens: [],
 			usuarios: [],
+			id: null,
 			usuario: {
 				nome: '',
 				email: ''
 			}
 		}
 	},
-	// created() {
-	// 	this.$http.post('usuario.json', {
-	// 		nome: 'Maria',
-	// 		email: 'maria_maria@gmail.com'
-	// 	}).then(res => {
-	// 		console.log(res)
-	// 	})
-	// },
 	methods: {
-		salvar() {
-			this.$http.post('usuario.json', this.usuario)
-			.then(res => {
-				this.usuario.nome = ''
-				this.usuario.email = ''
-			})
-			.catch(err => console.log(err.message))
+		limpar() {
+			this.usuario.nome = ''
+			this.usuario.email = ''
+			this.id = null
+			this.mensagens = []
 		},
-		obterUsuarios () {
-			// Tipo get
-			this.$http('usuario.json')
-			.then(res => {
+		carregar(id) {
+			this.id = id
+			this.usuario = { ...this.usuarios[id] }
+		},
+		excluir(id) {
+			this.$http.delete(`/usuarios/${id}`)
+				.then(() => this.limpar())
+				.catch(err => {
+					this.limpar()
+					this.mensagens.push({
+						texto: 'Problema para excluir!',
+						tipo: 'danger'
+					})
+				})
+		},
+		salvar() {
+			const metodo = this.id ? 'patch' : 'post'
+			const finalUrl = this.id ? `/${this.id}.json` : '.json'
+			this.$http[metodo](`/usuarios${finalUrl}`, this.usuario)
+				.then(_ => {
+					this.limpar()
+					this.mensagens.push({
+						texto: 'Operação realizada com sucesso!',
+						tipo: 'success'
+					})
+				})
+		},
+		obterUsuarios() {
+			this.$http.get('usuarios.json').then(res => {
 				this.usuarios = res.data
-				console.log(this.usuarios)
+				console.log(res.data)
 			})
-			.catch(err => console.log(err.message))
 		}
 	}
+	// created() {
+	// 	this.$http.post('usuarios.json', {
+	// 		nome: 'Maria',
+	// 		email: 'maria_maria@gmail.com'
+	// 	}).then(res => console.log(res))
+	// }
 }
 </script>
 
--- Projetos/http-exercicios/src/plugins/axios.js
+++ Projetos/http-exercicios/src/plugins/axios.js
@@ -2,9 +2,8 @@ import Vue from 'vue'
 import axios from 'axios'
 
 // axios.defaults.baseURL = 'https://vue-axios-5475a.firebaseio.com/'
-axios.defaults.headers.commom['Authorization'] = 'abc123' 
-// axios.defaults.headers.get['Accepts'] = 'application/json' 
-
+// axios.defaults.headers.common['Authorization'] = 'abc123'
+// axios.defaults.headers.get['Accepts'] = 'application/json'
 
 Vue.use({
     install(Vue) {
@@ -12,24 +11,22 @@ Vue.use({
         Vue.prototype.$http = axios.create({
             baseURL: 'https://vue-axios-5475a.firebaseio.com/',
             headers: {
-                'Authorization': "acb123"
+                "Authorization": "abc123"
             }
         })
+
         Vue.prototype.$http.interceptors.request.use(config => {
-            
+            console.log(config.method)
             // if(config.method == 'post') {
             //     config.method = 'put'
             // }
-            console.log(config.method)
             return config
         }, error => Promise.reject(error))
 
-        // Mudar os dados antes de receber o valor deles no front
-        Vue.prototype.$http.interceptors.respose.use(res => {
+        Vue.prototype.$http.interceptors.response.use(res => {
             // const array = []
-
-            // for (let chave in res.data) {
-            //     array.push({ id: chave, ...res.data[chave]})
+            // for(let chave in res.data) {
+            //     array.push({ id: chave, ...res.data[chave] })
             // }
 
             // res.data = array
