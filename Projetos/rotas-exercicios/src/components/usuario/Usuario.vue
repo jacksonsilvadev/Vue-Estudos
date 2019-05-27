906e4a683657e35b768399d7d6ea14fca69bfe8e
Jackson Silva <jackson.rodrigues9@hotmail.com>
2019-05-27 19:52:50 -0300

    Aula de Vue-Router

---
+++ Projetos/rotas-exercicios/src/components/usuario/Usuario.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="usuario">
+        <h2>Usuario</h2>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ Projetos/rotas-exercicios/src/router.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Inicio from './components/Inicio.vue'
+import Usuario from './components/usuario/Usuario.vue'
+
+Vue.use(Router);
+
+export default new Router({
+    mode:'history',
+    routes: [{
+        path: '/',
+        component: Inicio
+    },
+    {
+        path: '/usuario',
+        component: Usuario
+    }]
+})
