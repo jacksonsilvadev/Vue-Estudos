7e8de38c5440b01cfff288744755c53b107fe23d
Jackson <jackson.rodrigues9@hotmail.com>
2019-05-28 14:56:45 -0300

    VueX state e getters

---
+++ Projetos/vuex-exercicios/src/components/Resumo.vue
@@ -0,0 +1,36 @@
+<template>
+  <Painel titulo="Resumo" roxo>
+    <div class="resumo">
+      <span>
+        Total:
+        <strong>{{ total | dinheiro }}</strong>
+      </span>
+      <hr>
+      <button>Finalizar!</button>
+    </div>
+  </Painel>
+</template>
+
+<script>
+export default {
+  computed: {
+    total() {
+      return this.$store.getters.valorTotal;
+    },
+    produtos() {
+      return this.$store.state.produtos;
+    }
+  }
+};
+</script>
+
+<style>
+table {
+  width: 100%;
+}
+
+td {
+  border-top: 1px solid #eee;
+  width: 33%;
+}
+</style>
---
+++ Projetos/vuex-exercicios/src/store/store.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    state: {
+        produtos: []
+    },
+    getters: {
+        valorTotal(state) {
+            return state.produtos.map(p => p.quantidade * p.preco)
+                .reduce((total, atual) => total + atual, 0);
+        }
+    }
+})
