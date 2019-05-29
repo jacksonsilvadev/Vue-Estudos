f0cdcf1d72c57be4e837321373b5a8c6a602a178
Jackson <jackson.rodrigues9@hotmail.com>
2019-05-29 15:01:26 -0300

    Vuex store de portfolio

---
+++ Projetos/stock-trader/src/store/modules/portfolios.js
@@ -0,0 +1,53 @@
+export default {
+    state: {
+        funds: 10000,
+        stocks: []
+    },
+    mutations: {
+        buyStock(state, order) {
+            const record = state.stocks.find(element => element.id == order.stockId)
+
+            if (record) {
+                record.quantity += order.quantity
+            } else {
+                state.stocks.push({
+                    id: order.stockId,
+                    quantity: order.quantity
+                })
+            }
+            state.fund -= order.stockPrice * order.quantity
+        },
+        sellStock(state, order) {
+            const record = state.stocks.find(element => element.id == order.stockId)
+            if (record.quantity > order.quantity) {
+                record.quantity -= order.quantity
+            } else {
+                state.stocks.splice(state.stocks.indexOf(record), 1)
+            }
+            state.funds += order.stockPrice * order.quantity
+        }
+    },
+    actions: {
+        sellStock({
+            commit
+        }, order) {
+            commit('sellStock', order)
+        }
+    },
+    getters: {
+        stockPortfolio(state, getters) {
+            return state.stocks.map(stock => {
+                const record = getters.stocks.find(element => element.id == stock.id)
+                return {
+                    id: stock.id,
+                    quantity: stock.quantity,
+                    name: record.name,
+                    price: record.price
+                }
+            })
+        },
+        funds(state) {
+            return state.funds
+        }
+    }
+}
