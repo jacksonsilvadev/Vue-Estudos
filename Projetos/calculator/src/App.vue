6eb578b063db45e7ce087461b1fa321b7b3bf587
Jackson Silva <jackson.rodrigues9@hotmail.com>
2019-05-30 21:31:40 -0300

    Primeira aula de VueJs calculator projeto

---
+++ Projetos/calculator/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>Olá Vue</h1>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'app',
+  
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
